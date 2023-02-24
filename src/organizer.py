a150d4fe6131444ba6984faafc5992c7b6b079c1
Sadık Emre <70113249+sadikemreduzgun@users.noreply.github.com>
2023-02-24 18:31:59 +0530

    Beutify the code

--- src/organizer.py
+++ src/organizer.py
@@ -1,4 +1,5 @@
-from reach_selection_data import *
+# import package and library
+from reach_job_instance import *
 import numpy as np
 
 
@@ -8,27 +9,29 @@ def curly_organizer(string, selection="{job=node-exporter}", step="5s"):
     # create a string to return
     hold_str = ""
     # booleans for operations
-
+    # run once and if searched item is found, delete it.
     boole1 = False
     boole2 = False
 
     # look at letters one by one and organize
     for letter in string:
-        # when "{" is encountered, deletes until "}" and places "selection" in
-        # for some space allocation for instances entered
+        # when "{" is encountered, delete until "}"
+        # then place in "selection"
         if letter == "{":
             boole1 = True
             pass
         elif letter=="[":
             boole2 = True
             pass
-
+        # place chosen step time into
         if boole1 or boole2:
 
             if letter == "}":
                 hold_str += str(selection)
                 boole1 = False
             elif letter == "]":
+                # after deleting [some time]
+                # place in desired time
                 hold_str += f"[{step}]"
                 boole2 = False
             else:
@@ -39,11 +42,12 @@ def curly_organizer(string, selection="{job=node-exporter}", step="5s"):
 
             hold_str += letter
 
-
+    # return the hold_str string after performing step choice of some query functions
     return hold_str
 
 
 # organizes the URL and fixes URL request errors
+# do it because sometimes it makes problem of request
 def organize_url(query,start,end,step="5s"):
     
     # define a string to order and return
@@ -51,23 +55,23 @@ def organize_url(query,start,end,step="5s"):
     # get some chars which create problems, 
     # change them to URL utf-8 characters
     for letter in query:
-
+        # if there is (") character, change it to "%22"
         if letter=="\"":
             url_str += "%22"
-
+        # if there is "+" character, change it to "%2B"
         elif letter== "+":
             url_str+= "%2B"
-
+        # if there is "*" character, change it to %2A
         elif letter=="*":
             url_str+="%2A"
 
 #        elif letter=="-":
 #            url_str+="%2D"
-
+        # if there is no character which we search, don't change and add
         else:
             url_str+= letter
 
-    # return ordered string
+    # return ordered string-url
     url_str = f"http://localhost:9090/api/v1/query_range?query={url_str}&start={start}&end={end}&step={step}"
     return url_str
 
@@ -77,106 +81,92 @@ def organize_instance(query, device = reach_device()[0]):
     
     # a boolean for searching the word
     found=False
+    # define an empty string to be used
     check_word = ""
-    # if searched word is at the start immediately finish
+    # if searched word is at the start immediately decide and finish
+    # if first 4 letters form node, return instance which is created
     if query[0:4] == "node":
-        return "node", "{instance="+f'{return_jobs_interfaces("node")}'+"}"
-
+        # return instance created by return_instance function
+        return "node", "{instance="+f'{return_instance("node")}'+"}"
+    # if first 7 letters form libvirt, return instance which is created
     if query[0:7] == "libvirt":
+        # return instance created bt return_instance function
         return "libvirt","{instance=" + f'{return_jobs_interfaces("libvirt")}'+ ",domain=" + f'"{device}"'+"}"
 
     
     # if not continue searching
     for char in query:
-        # as we know if word is not at the start, it is after a "("
+        # as we know if word is not at the start, it is after a "(" because it must be a query function
         if char == "(":
+            # search for "(" if found, step on this execution of for loop
             found=True
             continue
-        # if it is found add it
+        # search letters after "(" character
         if found:
             
             check_word += char
             
-        # as the word return instance data
+        # if "node" is found, return instance as stated
         if check_word == "node":
             
             return "node", "{instance="+f'{return_jobs_interfaces("node")}'+"}"
 
-        
+        # if "libvirt" is found, return instance as stated
         if check_word == "libvirt":
             
             return  "libvirt", "{instance=" + f'{return_jobs_interfaces("libvirt")}'+ ",domain=" + f'"{device}"'+"}"
-    return 0, 0
+    # if can't be found return error
+    return -1, -1
 
 
-# get parameters and create an array to be loaded at a dataframe
+# organize libvirt data
 def organize_dataframe(query, count, boole, temp_data, if_same,boole2,device_num=0,incount=0,temp_metrics=0,boole3 = True,save=0,saves=0,urs ="",lists=[]):
     
-    # if there is no data, dont start
     if len(query['data']['result']) == 0:
-        continue
+        pass
     
-    # as soon as reach some data, start
     else:
         count+=1
-        # reach metric values
         data = query['data']['result'][0]['values']
-        #print(data)
-        
-        # create an array to be processed
-        data = np.array(data)
-        #print("\n\nQUERY WAS:\t\n", df.iloc[count,1])
 
-        # get metric data
+        data = np.array(data)
+
         metric  = data[:,1][np.newaxis]
 
-        # get time stamp data
         time_stamp = data[:,0][np.newaxis]
-        #metric = metric.apply(lambda x: GiB(float(x)), axis=1)
-        # for executing just once for 1. loop
+        # for executing just once
         if boole:
 
             if incount == 4:
-                
                 boole=False
                 
-            # concat time stamp and metrics values next to each other
+                          
             temp_data = np.concatenate((time_stamp.T,metric.T),axis=1)
             #hold = 0
-            # for executing just once to save first data
             if boole2:
-                    
                     save = temp_data
                     boole2=False
                     lists.append(urs)
-            
-            # for rest of execution, concat saved data vertically
+                
             else:
-                   
                     save = np.concatenate((save, temp_data), axis=0)
             
-            #print(save.shape)
+            print(save.shape)
         # merge data collectively
         
         #elif incount<5:
-        # generally it is executed
         else:
-            # for executing once by saving first data
+
             if boole3:
-                
                 saves = metric.T 
                 boole3 = False
-            
-            # connect metrics data of different VMs vertically
+                
             else:
-                
                 saves = np.concatenate((saves,metric.T),axis = 0)
-            
-            # connecting vertically connacted data
+                    
             if incount == 4:
-                
                 save = np.concatenate((save,saves), axis = 1)
                 lists.append(urs)
 
-    # return processed data, for saving and returning after process
+
     return temp_data, count, boole, temp_metrics, boole2, boole3, save, saves, lists
