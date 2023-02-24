0cf0d4192dc3fb23661680648426bce8f27f5c13
Sadık Emre <70113249+sadikemreduzgun@users.noreply.github.com>
2023-02-24 15:15:37 +0530

    Update main.py
    an update for keeping pace with queries

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ import requests as rq
 import numpy as np
 
 
-df = pd.read_csv('queries.csv')
+df = pd.read_csv('all_queries.csv')
 start = "2023-02-21T10:59:25.479Z"
 end = "2023-02-21T19:59:25.479Z"
 step = "5s"
@@ -12,17 +12,18 @@ boole = True
 devices = reach_device()
 save = 0
 saves = 0
+boole6=True
 print(devices)
 temp_data = 0
 count = -4
-titles = ['time_stamp']
 hold_query = df.iloc[0,1]
 mets= 0
 lastk = 12
 boole_node = True
 sws = 0
-
-for query in df.iloc[:,1]:
+titles = ["time_stamp"]
+listr = ["time_stamp"]
+for query in df.iloc[:,2]:
     boole2=True
     boole3 = True
 
@@ -32,6 +33,7 @@ for query in df.iloc[:,1]:
     if check == "libvirt":
         in_count = 1
         for device in devices:
+            print("sadasd: ", in_count)
             temp, instance = organize_instance(query, device)
             metrics = curly_organizer(query, instance, step)
             metrics = organize_url(metrics, start, end)
@@ -41,38 +43,64 @@ for query in df.iloc[:,1]:
             hold = count
             
             if_same = (hold_query == query)
-            
+                        
             print(if_same)
-            temp_data, count, boole, mets, boole2, boole3,save,saves = organize_dataframe(metrics,count ,boole, temp_data, if_same,boole2, len(devices),in_count, mets, boole3,save,saves)
+            temp_data, count, boole, mets, boole2, boole3,save,saves,listr = organize_dataframe(metrics,count ,boole, temp_data, if_same,boole2, len(devices),in_count, mets, boole3,save,saves,query,listr)
             print(hold,count)
             if hold != count:
                 try:
-                    titles.append(device +" : " + df.iloc[count,0])
+                    pass
+                    #titles.append(device +" : " + df.iloc[count,0])
                 except:
                     print("whooooa!")
+            
             in_count += 1
+        titles.append(query)
 
         
     if check =="node":
+
         temp, instance = organize_instance(query)
         metrics = curly_organizer(query, instance, step)
         metrics = organize_url(metrics, start, end)
         hold = metrics
         metrics = rq.get(metrics)
-        metrics = metrics.json()
+        data = metrics.json()
+        try: 
+            if len(data['data']['result']) == 0:
+                continue
+        except:
+            continue
+        #sws, boole_node = organize_node_df(metrics, boole_node, sws)
+        all_data = np.array(data['data']['result'][0]['values'])
+    
+        # get metric data
+        metric  = all_data[:,1][np.newaxis]
+        # get time stamp data
+        time_stamp = all_data[:,0][np.newaxis]
+    #metric = metric.apply(lambda x: GiB(float(x)), axis=1)
+    # for executing just once
+        if boole6:
+            temp_data2 = np.concatenate((time_stamp.T,metric.T),axis=1)
+            boole6=False
 
-        sws, boole_node = organize_node_df(metrics, boole_node, sws)
-
-        print(hold,count)
-        if hold != count:
-            try:
-                titles.append(device +" : " + df.iloc[count,0])
-            except:
-                print("whooooa!")
+    # merge data collectively
+        else:
+    
+            temp_data2 = np.concatenate((temp_data2, metric.T), axis=1)
+            print(hold,count)
+    #    if hold != count:
+     #       try:
+      #          titles.append(device +" : " + df.iloc[count,0])
+       #     except:
+        #        print("whooooa!")
     
     hold_query = query
 
 
+df = pd.DataFrame(temp_data2)
+df.to_csv('nodedede.csv')
+print(listr)
 # start title list with one title which is the same for all
 # a function to convert byte into megabyte
 def div(x):
@@ -81,7 +109,24 @@ def div(x):
     y=y+"Mb"
     return y
 
+print(len(titles))
+print(titles)
 # get data into dataframe object
-dataframe = pd.DataFrame(save)
+#dataframe = pd.DataFrame(save)
+try:
+    dataframe = pd.DataFrame(save, columns = listr)
+#print(titles)
+# turn time stamp data into clear data
+#dataframe["DateTime"] = dataframe.apply(lambda x: datetime.fromtimestamp(float(x["time_stamp"])), axis=1)
 
-dataframe.to_csv('last_state.csv')
+# turn byte data into mb data
+#for cols in dataframe.columns:
+#    if (cols !=  "time_stamp") and (cols != "DateTime"):
+        # usage of created div function
+#        dataframe[cols] = dataframe.apply(lambda y: div(float(y[cols])), axis = 1)
+
+# save dataframe into new created cs
+    dataframe.to_csv('last_state.csv')
+
+except:
+    pass
