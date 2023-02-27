abf2b4766db9f1126cfa72cf728401d7b751695a
Sadık Emre <70113249+sadikemreduzgun@users.noreply.github.com>
2023-02-27 13:18:29 +0530

    Update reach_time.py

--- src/reach_time.py
+++ src/reach_time.py
@@ -1,7 +1,9 @@
 from datetime import datetime, timedelta
+import json
+import requests as rq
 
 
-def give_default_dates(days=2):
+def give_default_dates(days=1):
     # get date one day ago
     currentDateAndTime = datetime.now() - timedelta(days)
     # get date 1 day and 5 mins. ago
@@ -19,7 +21,25 @@ def give_default_dates(days=2):
     start = start + "T"
     start = start + str(current_minus_5_min.time())[0:12] + "Z"
 
-    # return processed dates
+    query = "libvirt_domain_block_stats_allocation"
+    url_lib = f"http://localhost:9090/api/v1/query_range?query={query}&start={start}&end={end}&step=3m"
+    query="node_load1"
+    url_node = f"http://localhost:9090/api/v1/query_range?query={query}&start={start}&end={end}&step=30s"
+
+    # get data using api
+    data_node = rq.get(url_node)
+    # turn data into a dictionary
+    data_node = data_node.json()
+    # get data using api
+    data_lib = rq.get(url_lib)
+    # turn data into a dictionary
+    data_lib = data_lib.json()
+
+    # find a date which gives data recursively 
+    if len(data_node['data']['result']) == 0 or len(data_lib['data']['result']) == 0:
+
+        return give_default_dates(days=days+1)
+    # return processed dates             
     return start, end
 
 
