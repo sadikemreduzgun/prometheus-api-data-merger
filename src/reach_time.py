5b4f6a70bf5c8cc5df968eb2cfc9d39f4db201d1
Sadık Emre <70113249+sadikemreduzgun@users.noreply.github.com>
2023-03-13 07:18:32 +0000

    Update reach_time.py

--- src/reach_time.py
+++ src/reach_time.py
@@ -27,7 +27,7 @@ def give_default_dates(day_back=0, hour_back=0, min_back=30):
     # turn end date into the format which is used on url
     start = start + "T"
     start = start + str(start_time.time())[0:12]+"."+str(start_time.microsecond) + "Z"
-    print(start)
+    #print(start)
     query = "libvirt_domain_block_stats_allocation"
     url_lib = f"http://localhost:9090/api/v1/query_range?query={query}&start={start}&end={end}&step=3m"
     query="node_load1"
@@ -41,12 +41,12 @@ def give_default_dates(day_back=0, hour_back=0, min_back=30):
     data_lib = rq.get(url_lib)
     # turn data into a dictionary
     data_lib = data_lib.json()
-    print(len(data_node['data']['result']))
+    #print(len(data_node['data']['result']))
     
     #print(data_lib['data']['result'][0]['values'])
     # find a date which gives data recursively by going recursive_hour_back hours back every time
     if len(data_node['data']['result']) == 0 or len(data_lib['data']['result']) == 0:
-        print("no data")
+        #print("no data")
         #hour_random = random.randint(3,24)
         #min_random = random.randint(0,60)
         
